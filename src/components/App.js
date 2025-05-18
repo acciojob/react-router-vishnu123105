31ebc1270f0646981cd8421f2072ddac07d5abd9
Vishnu <bandarivishnu143@gmail.com>
2025-05-18 11:28:17 +0530

    don

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 
 const App = () => {
+  const About = () => (
+    <>
+      <h1>About</h1>
+      <p1>This is a sample React Router program.</p1>
+    </>
+  );
+  const Home = () => <h1>Welcome to my website!</h1>;
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <BrowserRouter>
+      <>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
